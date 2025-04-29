7eb108cd80cbd5acd4c6660485424f1481d18c5d
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2025-04-29 16:46:37 +0530

    user contants routes in useNavigation hook

--- Frontend/src/hooks/useNavigation.ts
+++ Frontend/src/hooks/useNavigation.ts
@@ -1,18 +1,18 @@
 import { useNavigate } from "react-router-dom"
-
+import { ROUTES } from "../constants/routes";
 
 const useNavigation = () => {
 
   const navigate = useNavigate();
 
   return {
-    goToLogin: () => navigate('/login'),
-    goToAdminLogin: () => navigate('/admin/login'),
-    goToRegister: () => navigate('/register'),
-    goToHome: () => navigate('/'),
-    goToSurveyForm: () => navigate('/survey-form'),
-    goToSurveySubmissions: () => navigate('/survey-submissions'),
-    goToDashboard: () => navigate('/admin/dashboard'),
+    goToLogin: () => navigate(ROUTES.USER.LOGIN),
+    goToAdminLogin: () => navigate(ROUTES.ADMIN.LOGIN),
+    goToRegister: () => navigate(ROUTES.USER.REGISTER),
+    goToHome: () => navigate(ROUTES.USER.HOME),
+    goToSurveyForm: () => navigate(ROUTES.USER.SURVEY_FORM),
+    goToSurveySubmissions: () => navigate(ROUTES.USER.SURVEY_SUBMISSIONS),
+    goToDashboard: () => navigate(ROUTES.ADMIN.DASHBOARD),
     goBack: () => navigate(-1),
     navigateTo: (path: string) => navigate(path),
   }
